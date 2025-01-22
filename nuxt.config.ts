a2f62d17a3b0443716f1e6ca729ddc54a1a3ad0a
Mark de Neut Brossois <mark.dnb@live.nl>
2025-01-22 19:53:09 +0100

    get it from public now please

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      link: [{ rel: 'icon', type: 'image/png', href: "favicon.png" }]
+      link: [{ rel: 'icon', type: 'image/png', href: "/favicon.png" }]
     }
   }
 })
